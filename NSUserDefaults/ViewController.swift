14f8a9a0739f5905438d0417bfe53e0a13285121
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-08 17:50:16 +0900

    UserDefaults.standardを使って値の保存と取り出しをやってみた

--- NSUserDefaults/ViewController.swift
+++ NSUserDefaults/ViewController.swift
@@ -3,17 +3,34 @@
 //  NSUserDefaults
 //
 //  Created by IwasakIYuta on 2021/07/08.
-//
+//https://qiita.com/uhooi/items/429cac9b798b9c0937ae
+//CRUD操作
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // String?で返すのであんラップしてる
+        //保存してる値をforKeyを指定して取得
+        if let text1 = UserDefaults.standard.string(forKey: "text1") {
+            //値が保存されてる時にそのまま値を表示する
+            textField.text = text1
+        }
     }
-
-
+    //editingChangedにすることで変更を検出することができる
+    @IBAction func editingChanged(_ sender: Any) {
+        print(textField.text ?? "")
+    }
+    //UserDefaultsにStringを保存してみる
+    @IBAction func seveButton(_ sender: UIButton) {
+        //textField.textをforKeyで指定して保存する
+        UserDefaults.standard.set(textField.text, forKey: "text1")
+    }
+    
 }
 
